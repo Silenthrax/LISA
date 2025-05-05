8b4bb9895aaafff1c7b412fa3d68e2fc82a9ba41
UFCMUSIC <babymusic936@gmail.com>
2025-05-05 14:13:19 +0530

    Update helper.py

--- SONALI/utils/stuffs/helper.py
+++ SONALI/utils/stuffs/helper.py
@@ -111,7 +111,7 @@ Admins only:
  ❍ /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
  ❍ /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
 __
-𝐒ᴘᴇᴄɪᴀʟ 𝐂ᴏᴍᴍᴀɴᴅs 𝐒ᴜᴘᴘᴏʀᴛ 𝐀ʟʟ 𝐄xᴀᴍᴘʟᴇ  -  Sonali 𝚋𝚊𝚗 Sonali 𝚖𝚞𝚝𝚎 Sonali 𝚙𝚛𝚘𝚖𝚘𝚝𝚎 ..... 𝚎𝚝𝚌
+𝐒ᴘᴇᴄɪᴀʟ 𝐂ᴏᴍᴍᴀɴᴅs 𝐒ᴜᴘᴘᴏʀᴛ 𝐀ʟʟ 𝐄xᴀᴍᴘʟᴇ  -  Tᴀᴍᴀɴɴᴀ Bᴀɴ Tᴀᴍᴀɴɴᴀ Mᴜᴛᴇ Tᴀᴍᴀɴɴᴀ Pʀᴏᴍᴏᴛᴇ ..... 𝚎𝚝𝚌
 '''
     HELP_Search = '''Sᴇᴀʀᴄʜ
 
